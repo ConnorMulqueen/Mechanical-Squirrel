a777edadb7dc9e924be60f5e70ede122e02f1ecd
ConnorMulqueen <connor.mulqueen@gmail.com>
2019-06-09 11:20:02 -0400

    change document header

--- docs/website/src/app/app.component.ts
+++ docs/website/src/app/app.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'website';
+  public constructor(private titleService: Title ) {
+    this.titleService.setTitle('Mechanical Squirrel');
+  }
 }
--- docs/website/src/app/app.module.ts
+++ docs/website/src/app/app.module.ts
@@ -1,11 +1,9 @@
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserModule, Title } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MatToolbarModule, MatButtonModule, MatSelectModule, MatIconModule, MatCardModule } from '@angular/material';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatVideoModule } from 'mat-video';
 
 @NgModule({
   declarations: [
@@ -19,10 +17,11 @@ import { MatVideoModule } from 'mat-video';
     MatSelectModule,
     MatIconModule,
     MatCardModule,
-    BrowserAnimationsModule,
-    MatVideoModule
+    BrowserAnimationsModule
+    ],
+  providers: [
+    Title
   ],
-  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
